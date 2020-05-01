757b519ee07357245dafa43a9645e0af646b546f
Romilly Cocking <romilly.cocking@gmail.com>
2020-05-01 17:52:11 +0100

    Image localisation is working.

--- src/hamcrest_helpers/files.py
+++ src/hamcrest_helpers/files.py
@@ -3,6 +3,10 @@ import os
 from hamcrest.core.base_matcher import BaseMatcher, T
 from hamcrest.core.description import Description
 
+def read(f):
+    with open(f) as input_file:
+        return input_file.read()
+
 
 class EmptyDirectoryMatcher(BaseMatcher):
     def describe_mismatch(self, item: T, mismatch_description: Description) -> None:
@@ -57,5 +61,30 @@ class DirectoryContentsMatcher(BaseMatcher):
         else:
             mismatch_description.append_text(' which contains %s' % ', '.join(self.get_contents(item)))
 
+
 def contains_files(*paths):
     return DirectoryContentsMatcher(paths)
+
+
+class FileContentMatcher(BaseMatcher):
+    def describe_mismatch(self, item: T, mismatch_description: Description) -> None:
+        if not os.path.isfile(item):
+            mismatch_description.append_text('%s was not a file' % item)
+        else:
+            mismatch_description.append_text('a file with contents %s' % read(item))
+
+    def _matches(self, item: T) -> bool:
+        if not os.path.isfile(item):
+            return False
+        return self.content_matcher.matches(read(item))
+
+    def describe_to(self, description: Description) -> None:
+        description.append_text('a file with contents ')
+        self.content_matcher.describe_to(description)
+
+    def __init__(self, matcher: BaseMatcher):
+        self.content_matcher = matcher
+
+
+def file_content(matcher):
+    return FileContentMatcher(matcher)
